d0a07837dd7271aa90dea7f3c16e041d54eef7ee
autodocs <autodocs>
2018-11-01 07:07:50 +0000

    build based on f82562f

---
+++ dev/search_index.js
@@ -0,0 +1,43 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "GeoRasters.jl Documentation",
+    "title": "GeoRasters.jl Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#GeoRasters.get_nodata-Tuple{Ptr{Nothing}}",
+    "page": "GeoRasters.jl Documentation",
+    "title": "GeoRasters.get_nodata",
+    "category": "method",
+    "text": "Retrieves nodata value from RasterBand.\n\n\n\n\n\n"
+},
+
+{
+    "location": "#GeoRasters.mask_flags-Tuple{Int32}",
+    "page": "GeoRasters.jl Documentation",
+    "title": "GeoRasters.mask_flags",
+    "category": "method",
+    "text": "Takes bitwise OR-ed set of status flags and returns flags.\n\n\n\n\n\n"
+},
+
+{
+    "location": "#GeoRasters.jl-Documentation-1",
+    "page": "GeoRasters.jl Documentation",
+    "title": "GeoRasters.jl Documentation",
+    "category": "section",
+    "text": "Modules = [GeoRasters]\nOrder   = [:function, :type]"
+},
+
+{
+    "location": "#Index-1",
+    "page": "GeoRasters.jl Documentation",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+]}
