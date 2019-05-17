2090578b464f84bded22ec549cac9abe49eb09ec
Maarten Pronk <git@evetion.nl>
2019-05-17 19:43:09 +0200

    Added test for flipping upside down.

--- test/test_geoutils.jl
+++ test/test_geoutils.jl
@@ -1,4 +1,16 @@
 @testset "GeoUtils" begin
-    ga = GeoRasters.read("data/utmsmall.tif")
-    @test GeoRasters.bbox(ga) == (min_x = 440720.0, min_y = 3.74532e6, max_x = 446720.0, max_y = 3.75132e6)
+    @testset "bbox" begin
+        ga = GeoRasters.read("data/utmsmall.tif")
+        @test GeoRasters.bbox(ga) == (min_x = 440720.0, min_y = 3.74532e6, max_x = 446720.0, max_y = 3.75132e6)
+    end
+
+    @testset "flip upside down" begin
+        ga = GeoRasters.read("data/utmsmall.tif")
+        ga_identical = GeoRasters.read("data/utmsmall.tif")
+        @test ga == ga_identical
+        GeoRasters.flipud!(ga)
+        @test ga != ga_identical
+        GeoRasters.flipud!(ga)
+        @test ga == ga_identical
+    end
 end
