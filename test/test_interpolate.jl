dedb28d66631776428fdff94015acaca432365e0
Martijn Visser <mgvisser@gmail.com>
2019-05-17 15:05:47 +0200

    gitignore test data

--- test/test_interpolate.jl
+++ test/test_interpolate.jl
@@ -5,9 +5,9 @@ using StaticArrays
     @testset "Regular Grid interpolation" begin
         ga = GeoArray(Array{Union{Missing, Float64}}(rand(10, 10)))
         ga.A[2,2] = missing
-        @test sum(ismissing.(ga)) == 1
+        @test count(ismissing, ga) == 1
         GeoRasters.interpolate!(ga, Kriging())
-        @test sum(ismissing.(ga)) == 0
+        @test count(ismissing, ga) == 0
     end
     @testset "Irregular Grid interpolation (not yet supported)" begin
         ga = GeoArray(Array{Union{Missing, Float64}}(rand(10, 10, 1)), GeoRasters.geotransform_to_affine(SVector(0.,1.,1.,0.,0.,1.)), "")
