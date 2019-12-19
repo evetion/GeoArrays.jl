1e15615fbe6e6019038fbeb9081ccc2c302e7864
ASUS1 <crghilardi@yahoo.com>
2019-12-19 14:30:12 -0800

    add +,*,/ methods with tests and add using CoordinateTransfomations to test affine/crs

--- src/GeoArrays.jl
+++ src/GeoArrays.jl
@@ -25,6 +25,6 @@ export interpolate!
 
 export epsg!
 
-export -
+export -,+,*,/
 
 end
--- src/operations.jl
+++ src/operations.jl
@@ -2,5 +2,26 @@ function Base.:-(a::GeoArray, b::GeoArray)
     size(a) == size(b) || throw(DimensionMismatch("The sizes of a and b do not match."))
     a.f == b.f || error("The affine information does not match")
     a.crs == b.crs || error("The crs information does not match")
-    GeoArray(a.A - b.A,a.f,a.crs)
+    GeoArray(a.A .- b.A,a.f,a.crs)
+end
+
+function Base.:+(a::GeoArray, b::GeoArray)
+    size(a) == size(b) || throw(DimensionMismatch("The sizes of a and b do not match."))
+    a.f == b.f || error("The affine information does not match")
+    a.crs == b.crs || error("The crs information does not match")
+    GeoArray(a.A .+ b.A,a.f,a.crs)
+end
+
+function Base.:*(a::GeoArray, b::GeoArray)
+    size(a) == size(b) || throw(DimensionMismatch("The sizes of a and b do not match."))
+    a.f == b.f || error("The affine information does not match")
+    a.crs == b.crs || error("The crs information does not match")
+    GeoArray(a.A .* b.A,a.f,a.crs)
+end
+
+function Base.:/(a::GeoArray, b::GeoArray)
+    size(a) == size(b) || throw(DimensionMismatch("The sizes of a and b do not match."))
+    a.f == b.f || error("The affine information does not match")
+    a.crs == b.crs || error("The crs information does not match")
+    GeoArray(a.A ./ b.A,a.f,a.crs)
 end
