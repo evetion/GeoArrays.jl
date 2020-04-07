12a055479c6a2966b7b5e2615bb20e45c3c8c29c
Maarten Pronk <git@evetion.nl>
2020-04-07 12:17:33 +0200

    Fixed flipud! and a geotransform conversion. Updated to latest GeoStatsBase.

--- src/interpolate.jl
+++ src/interpolate.jl
@@ -9,7 +9,7 @@ function interpolate!(ga::GeoArray, solver::T, band=1, symbol=:z) where T<:Abstr
 
     # Regular grid
     else
-        problemdata = RegularGridData(Dict(symbol=>ga.A[:,:,band]), Tuple(ga.f.translation), (abs(ga.f.linear[1]), abs(ga.f.linear[4])))
+        problemdata = RegularGridData(OrderedDict(symbol=>ga.A[:,:,band]), Tuple(ga.f.translation), (abs(ga.f.linear[1]), abs(ga.f.linear[4])))
         problemdomain = RegularGrid(size(ga.A)[1:2], Tuple(ga.f.translation), (abs(ga.f.linear[1]), abs(ga.f.linear[4])))
     end
 
--- test/test_geoutils.jl
+++ test/test_geoutils.jl
@@ -10,7 +10,15 @@
         @test ga == ga_identical
         GeoArrays.flipud!(ga)
         @test ga != ga_identical
+        @test GeoArrays.bbox(ga) == (min_x = 440720.0, min_y = 3.74532e6, max_x = 446720.0, max_y = 3.75132e6)
         GeoArrays.flipud!(ga)
         @test ga == ga_identical
+    end
+
+    @testset "geotransform should remain the same" begin
+        gt = [1000.,1.,0.2,2000.,0.,1.]
+        f = GeoArrays.geotransform_to_affine(gt)
+        gtnew = GeoArrays.affine_to_geotransform(f)
+        @test gt == gtnew
     end
 end
