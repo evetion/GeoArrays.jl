70775cecd7f4c287a428f629a2b03eaf1a9512c3
Maarten Pronk <git@evetion.nl>
2020-05-26 22:19:21 +0200

    Replaced Kriging by package that doesn't throw dep hell on every upgrade.

--- test/test_interpolate.jl
+++ test/test_interpolate.jl
@@ -1,23 +1,23 @@
-using KrigingEstimators
 using StaticArrays
+using InverseDistanceWeighting
 
 @testset "Interpolating rasters" begin
     @testset "Regular Grid interpolation" begin
-        ga = GeoArray(Array{Union{Missing, Float64}}(rand(10, 10, 1)), GeoArrays.geotransform_to_affine(SVector(0.,1.,0.,0.,0.,1.)), "")
+        ga = GeoArray(Array{Union{Missing,Float64}}(rand(10, 10, 1)), GeoArrays.geotransform_to_affine(SVector(0., 1., 0., 0., 0., 1.)), "")
         ga.A[2,2,1] = missing
         @test count(ismissing, ga) == 1
-        GeoArrays.interpolate!(ga, Kriging())
+        GeoArrays.interpolate!(ga, InvDistWeight())
         @test count(ismissing, ga) == 0
     end
     @testset "Regular Grid interpolation with negative spacing" begin
-        ga = GeoArray(Array{Union{Missing, Float64}}(rand(10, 10, 1)), GeoArrays.geotransform_to_affine(SVector(0.,1.,0.,0.,0.,-1.)), "")
+        ga = GeoArray(Array{Union{Missing,Float64}}(rand(10, 10, 1)), GeoArrays.geotransform_to_affine(SVector(0., 1., 0., 0., 0., -1.)), "")
         ga.A[2,2,1] = missing
         @test count(ismissing, ga) == 1
-        GeoArrays.interpolate!(ga, Kriging())
+        GeoArrays.interpolate!(ga, InvDistWeight())
         @test count(ismissing, ga) == 0
     end
     @testset "Irregular Grid interpolation (not yet supported)" begin
-        ga = GeoArray(Array{Union{Missing, Float64}}(rand(10, 10, 1)), GeoArrays.geotransform_to_affine(SVector(0.,1.,1.,0.,0.,1.)), "")
-        @test_throws ErrorException GeoArrays.interpolate!(ga, Kriging())
+        ga = GeoArray(Array{Union{Missing,Float64}}(rand(10, 10, 1)), GeoArrays.geotransform_to_affine(SVector(0., 1., 1., 0., 0., 1.)), "")
+        @test_throws ErrorException GeoArrays.interpolate!(ga, InvDistWeight())
     end
 end
