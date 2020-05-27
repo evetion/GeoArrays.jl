c3161a8f2138e1b4257f98a9992813eef68eff02
Maarten Pronk <git@evetion.nl>
2020-05-27 10:59:55 +0200

    Seems that dep issue has been solved overnight.

--- test/test_interpolate.jl
+++ test/test_interpolate.jl
@@ -1,23 +1,23 @@
 using StaticArrays
-using InverseDistanceWeighting
+using KrigingEstimators
 
 @testset "Interpolating rasters" begin
     @testset "Regular Grid interpolation" begin
         ga = GeoArray(Array{Union{Missing,Float64}}(rand(10, 10, 1)), GeoArrays.geotransform_to_affine(SVector(0., 1., 0., 0., 0., 1.)), "")
         ga.A[2,2,1] = missing
         @test count(ismissing, ga) == 1
-        GeoArrays.interpolate!(ga, InvDistWeight())
+        GeoArrays.interpolate!(ga, Kriging())
         @test count(ismissing, ga) == 0
     end
     @testset "Regular Grid interpolation with negative spacing" begin
         ga = GeoArray(Array{Union{Missing,Float64}}(rand(10, 10, 1)), GeoArrays.geotransform_to_affine(SVector(0., 1., 0., 0., 0., -1.)), "")
         ga.A[2,2,1] = missing
         @test count(ismissing, ga) == 1
-        GeoArrays.interpolate!(ga, InvDistWeight())
+        GeoArrays.interpolate!(ga, Kriging())
         @test count(ismissing, ga) == 0
     end
     @testset "Irregular Grid interpolation (not yet supported)" begin
         ga = GeoArray(Array{Union{Missing,Float64}}(rand(10, 10, 1)), GeoArrays.geotransform_to_affine(SVector(0., 1., 1., 0., 0., 1.)), "")
-        @test_throws ErrorException GeoArrays.interpolate!(ga, InvDistWeight())
+        @test_throws ErrorException GeoArrays.interpolate!(ga, Kriging())
     end
 end
