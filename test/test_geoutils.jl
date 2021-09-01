e015d314b0f02ec1b4ec26bb7e0100ca4eae92db
Kristof Ostir <krostir@gmail.com>
2021-09-01 08:59:20 +0200

    Add BBox cropping

--- test/test_geoutils.jl
+++ test/test_geoutils.jl
@@ -33,4 +33,11 @@ const tbbox = (min_x = 440720.0, min_y = 3.74532e6, max_x = 446720.0, max_y = 3.
         gtnew = GeoArrays.affine_to_geotransform(f)
         @test gt == gtnew
     end
+
+    @testset "bbox overlapping" begin
+        @test GeoArrays.bbox_overlap((min_x = 10, min_y = 10, max_x = 1000, max_y = 1000), (min_x = 20, min_y = 20, max_x = 900, max_y = 1100)) == true
+        @test GeoArrays.bbox_overlap((min_x = 10, min_y = 10, max_x = 100, max_y = 100), (min_x = 200, min_y = 200, max_x = 400, max_y = 400)) == false
+        @test GeoArrays.bbox_overlap((min_x = 440720.0, min_y = 3.74532e6, max_x = 446720.0, max_y = 3.75132e6), (min_x = 442720.0, min_y = 3.94532e6, max_x = 456720.0, max_y = 3.75032e6)) == true
+        @test GeoArrays.bbox_overlap((min_x = 440720.0, min_y = 3.74532e6, max_x = 446720.0, max_y = 3.75132e6), (min_x = 342720.0, min_y = 3.94532e6, max_x = 356720.0, max_y = 3.75032e6)) == false
+    end
 end
