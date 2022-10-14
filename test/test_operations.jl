95722b71725fe12aff99e08c2916aef1ff2fc2e2
Maarten Pronk <git@evetion.nl>
2022-10-14 21:38:27 +0200

    Fix test.

--- test/test_operations.jl
+++ test/test_operations.jl
@@ -37,8 +37,15 @@ using CoordinateTransformations
         @test typeof(g / h).name.name == :GeoArray
 
         g = GeoArray([1 missing; 2 3])
-        @inferred gg = coalesce(g, 0)
-        @test gg.A[2] == 0
-        @test eltype(gg) == Int
+        @inferred coalesce(g, 0)
+        gg = coalesce(g, 0)
+        @test gg.A[3] == 0
+        @test eltype(gg) == Int64
+
+        g = GeoArray([1.0 missing; 2 3])
+        @inferred coalesce(g, Inf)
+        gg = coalesce(g, Inf)
+        @test gg.A[3] == Inf
+        @test eltype(gg) == Float64
     end
 end
