c0aa1c4230b7772f093baeffbe086feda5d2cd45
Maarten Pronk <git@evetion.nl>
2024-01-07 19:44:23 +0100

    Fix docs.

--- src/interpolate.jl
+++ src/interpolate.jl
@@ -1,3 +1,8 @@
+"""
+    fill!(ga::GeoArray, solver, band=1)
+
+Replace missing values in GeoArray `ga` using `solver`.
+"""
 function fill!(::GeoArray, solver, band)
     error("fill! using $solver is not implemented yet. Please use an `EstimationSolver` from the GeoStats.jl ecosystem.")
 end
